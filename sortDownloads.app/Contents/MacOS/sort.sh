e7d219faf417c18fdefff3c3bc4ed450c1251921
Stefan Grothkopp <sg@Detroit.local>
2014-08-01 15:27:22 +0200

    fixed moving files

--- sortDownloads.app/Contents/MacOS/sort.sh
+++ sortDownloads.app/Contents/MacOS/sort.sh
@@ -5,17 +5,13 @@ DIR=~/Downloads/
 
 for f in $FILES
 do
-  SRC=$(mdls "$f" -name kMDItemWhereFroms|tail -n2|head -n1|sed 's/^ *//')
+  SRC=$(mdls "$f" -name kMDItemWhereFroms|grep '"'|grep -v '""'|tail -n1|sed 's/^ *//')
 
-  if [ "$SRC" == '""' ] 
-  then
-   SRC=$(mdls "$f" -name kMDItemWhereFroms|tail -n3|head -n1)
-  fi
 
-  SRC=$(echo "$SRC" |sed 's/^ *".*\/\/\([^\/]*\).*$/\1/')
+  SRC=$(echo "$SRC" |sed 's/^ *"[^\/]*\/\/\([^\/]*\).*$/\1/')
   SRC=$(echo "$SRC" |sed 's/^www.//')
 
-  if [[ "$SRC" != *\(null\)* ]] && [[  "$SRC" != "$f" ]] && [[  -f "$f" ]]
+  if [[ "$SRC" != *\(null\)* ]] && [[  "$SRC" != "$f" ]] && [[  -f "$f" ]] && [[ "$SRC" != "" ]] 
   then
   echo "processing $f"
   echo "-$SRC-"
@@ -23,12 +19,28 @@ do
     if [ ! -d "$DIR$SRC" ]; then
       mkdir "$DIR$SRC"
     fi
+
+
+    file="${f##*/}"
+    ext="${file##*.}"
+    n="${file%.*}"
+
+    # Check if file exists and rename if necessary
+    name="$DIR$SRC/$n"
+    if [[ -e $name.$ext ]] ; then
+      i=1
+      while [[ -e "$name ($i).$ext" ]] ; do
+       let i++
+      done
+      name="$name ($i)"
+    fi
     
-    mv "$f" "$DIR$SRC"
+    mv "$f" "$name.$ext"
 
     SHORT=$(echo "$f"| sed "s#$HOME##"| sed 's#/Downloads/##')
-    osascript -e "display notification \"$SHORT ==>\n$SRC\" with title \"Sort Downloads\""
 
+    # Notification
+    osascript -e "display notification \"$SHORT ==>\n$SRC\" with title \"Sort Downloads\""
 
   fi
 
